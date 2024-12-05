d732c9e486cbd6aeac4ef6fd2fd8e8ccc1d454d1
Zhang <zelocrystal1@gmail.com>
2024-12-04 18:19:01 -0700

    added warning/success messages

--- app/controllers/toppings_controller.rb
+++ app/controllers/toppings_controller.rb
@@ -3,7 +3,7 @@ class ToppingsController < ApplicationController
 
   def show
   end
-  
+
   def index
     @toppings = Topping.all
   end
@@ -14,33 +14,46 @@ class ToppingsController < ApplicationController
 
   def create
     @topping = Topping.new(topping_params)
+  
     if @topping.save
-      redirect_to toppings_path, notice: 'Topping was successfully added.'
+      redirect_to toppings_path, notice: "#{@topping.name} was successfully added."
     else
-      render :new
+      if @topping.errors[:name].any?
+        flash[:alert] = 'Failed to add topping. Topping name must be unique.'
+      else
+        flash[:alert] = 'Failed to add topping. Please check your input.'
+      end
+      render :new, status: :unprocessable_entity
     end
   end
+  
 
   def edit
   end
 
   def update
     if @topping.update(topping_params)
-      redirect_to toppings_path, notice: 'Topping was successfully updated.'
+      redirect_to toppings_path, notice: "#{@topping.name} was successfully updated."
     else
-      render :edit
+      flash[:alert] = 'Failed to update topping. Please fix the errors below.'
+      render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @topping.destroy
-    redirect_to toppings_path , notice: 'Topping was successfully destroyed.'
+    if @topping.destroy
+      redirect_to toppings_path, notice: "#{@topping.name} was successfully removed."
+    else
+      redirect_to toppings_path, alert: 'Failed to delete topping. Please try again later.'
+    end
   end
 
   private
 
     def set_topping
       @topping = Topping.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to toppings_path, alert: 'Topping not found.'
     end
 
     def topping_params
