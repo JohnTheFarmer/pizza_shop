c724108db92fd9b6744410ad0589f4f70b6cf316
Zhang <zelocrystal1@gmail.com>
2024-12-05 00:19:48 -0700

    added testing data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,57 @@
 #   end
 
 
-User.create!(email: 'owner@test.com', password: 'password', role: 'owner')
-User.create!(email: 'chef@test.com', password: 'password', role: 'chef')
-User.create!(email: 'customer@test.com', password: 'password', role: 'customer')
+# Seeding users with Devise and ensuring no duplicates
+User.find_or_create_by!(email: 'owner@test.com') do |user|
+    user.password = 'password'
+    user.password_confirmation = 'password'
+    user.role = 'owner'
+  end
+  
+  User.find_or_create_by!(email: 'chef@test.com') do |user|
+    user.password = 'password'
+    user.password_confirmation = 'password'
+    user.role = 'chef'
+  end
+  
+  User.find_or_create_by!(email: 'customer@test.com') do |user|
+    user.password = 'password'
+    user.password_confirmation = 'password'
+    user.role = 'customer'
+  end
+
+
+
+# Create Toppings only if they do not exist
+pepperoni = Topping.find_or_create_by!(name: 'Pepperoni') { |t| t.description = 'Delicious pepperoni slices' }
+mushrooms = Topping.find_or_create_by!(name: 'Mushrooms') { |t| t.description = 'Fresh mushrooms' }
+olives = Topping.find_or_create_by!(name: 'Olives') { |t| t.description = 'Black olives' }
+bell_peppers = Topping.find_or_create_by!(name: 'Bell Peppers') { |t| t.description = 'Sweet bell peppers' }
+onions = Topping.find_or_create_by!(name: 'Onions') { |t| t.description = 'Sliced onions' }
+pineapple = Topping.find_or_create_by!(name: 'Pineapple') { |t| t.description = 'Sweet pineapple' }
+bbq_chicken = Topping.find_or_create_by!(name: 'BBQ Chicken') { |t| t.description = 'Grilled BBQ chicken' }
+
+# Create Pizzas only if they do not exist
+margherita = Pizza.find_or_create_by!(name: 'Margherita') do |pizza|
+  pizza.toppings << [pepperoni, mushrooms]  # Add toppings to Margherita
+end
+
+pepperoni_pizza = Pizza.find_or_create_by!(name: 'Pepperoni') do |pizza|
+  pizza.toppings << pepperoni  # Add only pepperoni
+end
+
+vegetarian_pizza = Pizza.find_or_create_by!(name: 'Vegetarian') do |pizza|
+  pizza.toppings << [bell_peppers, onions, olives, mushrooms]  # Multiple toppings for Vegetarian
+end
+
+hawaiian_pizza = Pizza.find_or_create_by!(name: 'Hawaiian') do |pizza|
+  pizza.toppings << pineapple  # Only pineapple topping
+end
+
+bbq_chicken_pizza = Pizza.find_or_create_by!(name: 'BBQ Chicken') do |pizza|
+  pizza.toppings << [bbq_chicken, onions]  # BBQ Chicken with BBQ Chicken and Onions
+end
+
+# Check if the data was added successfully
+puts "Created #{Pizza.count} pizzas and #{Topping.count} toppings."
+
