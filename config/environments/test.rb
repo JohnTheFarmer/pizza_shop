fc82e5d0b144681231f5d689c3c6c7f8c9671d7c
Zhang <zelocrystal1@gmail.com>
2024-12-06 18:28:28 -0700

    enabled eager loading for testing

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -50,4 +50,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Enable eager loading in test environment
+  config.eager_load = true
 end
